44788aa9990c93cd004107674192394b6394ce2f
Vlad <vlad.lapets@gmail.com>
2016-11-28 15:51:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var array = [];
 window.onload = function() {
 	var number = Math.floor(Math.random() * (100 - 10)) + 10;
 	document.getElementById('rand_number').innerHTML = number;
@@ -10,15 +11,22 @@ function randNumber() {
 }
 
 document.onkeydown = function(e) {
-	var array = [];
 	var you_number;
 	var key = e.charCode || e.keyCode || 0;
 	var input_number = String.fromCharCode(e.keyCode);
-	if((key >= 48 && key <= 57)) {
-		while(array.length < 2) {
+	if((key >= 48 && key <= 57) || (key >= 96 && key <= 105)) {
+		if(array.length < 2) {
 			array.push(input_number);
+			you_number = array[0];
+			document.getElementById('rand_number').innerHTML = you_number;
 		}
-		you_number = array[0].concat(array[1]);
+		if (array.length == 2) {
+			you_number = you_number.concat(array[1]);
+			document.getElementById('rand_number').innerHTML = you_number;
+		}
+		// else {
+		// 	array.splice(0, arr.length);
+		// 	// проверка на совпадение
+		// }
 	}
-	document.getElementById('rand_number').innerHTML = you_number;
 }
