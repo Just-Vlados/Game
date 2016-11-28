70cc3e6b4bd19adc4ad60a6f40f6678a1be99fec
Vlad <vlad.lapets@gmail.com>
2016-11-28 21:54:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
-var array = [];
+var inputArray = [];
+var numArray = []
+var you_number;
+
 window.onload = function() {
 	var number = Math.floor(Math.random() * (100 - 10)) + 10;
+	numArray.push(number);
 	document.getElementById('rand_number').innerHTML = number;
-	//$("#rand_number").innerHTML = number;
 }
 
 function randNumber() {
-	var number = Math.floor(Math.random() * (100 - 10)) + 10;
-	document.getElementById('rand_number').innerHTML = number;
+	var num = Math.floor(Math.random() * (100 - 10)) + 10;
+	numArray.push(num);
+	document.getElementById('rand_number').innerHTML = num;
 }
 
 document.onkeydown = function(e) {
-	var you_number;
 	var key = e.charCode || e.keyCode || 0;
 	var input_number = String.fromCharCode(e.keyCode);
 	if((key >= 48 && key <= 57) || (key >= 96 && key <= 105)) {
-		if(array.length < 2) {
-			array.push(input_number);
-			you_number = array[0];
+		if(inputArray.length < 2) {
+			inputArray.push(input_number);
+			you_number = inputArray[0];
 			document.getElementById('rand_number').innerHTML = you_number;
 		}
-		if (array.length == 2) {
-			you_number = you_number.concat(array[1]);
+		if (inputArray.length == 2) {
+			you_number = you_number.concat(inputArray[1]);
 			document.getElementById('rand_number').innerHTML = you_number;
+			inputArray.length = [];
 		}
-		// else {
-		// 	array.splice(0, arr.length);
-		// 	// проверка на совпадение
-		// }
 	}
+	if(you_number >= 10 && you_number <= 99) {
+		compareNumbers();
+	}
+}
+
+function compareNumbers() {
+	if(you_number == numArray[numArray.length - 2]) {
+		randNumber();
+	}
+	else {
+		alert("Lose");
+	}
+	
 }
