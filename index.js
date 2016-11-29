cedc5e27a86735a5427b6ebde5216b0ac2a97c34
Vlad <vlad.lapets@gmail.com>
2016-11-29 18:55:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,60 @@
 var inputArray = [];
 var numArray = []
 var you_number;
+var intervalId;
 
 window.onload = function() {
 	var number = Math.floor(Math.random() * (100 - 10)) + 10;
 	numArray.push(number);
 	document.getElementById('rand_number').innerHTML = number;
-}
 
-function randNumber() {
-	var num = Math.floor(Math.random() * (100 - 10)) + 10;
-	numArray.push(num);
-	document.getElementById('rand_number').innerHTML = num;
-}
 
-document.onkeydown = function(e) {
-	var key = e.charCode || e.keyCode || 0;
-	var input_number = String.fromCharCode(e.keyCode);
-	if((key >= 48 && key <= 57) || (key >= 96 && key <= 105)) {
-		if(inputArray.length < 2) {
-			inputArray.push(input_number);
-			you_number = inputArray[0];
-			document.getElementById('rand_number').innerHTML = you_number;
-		}
-		if (inputArray.length == 2) {
-			you_number = you_number.concat(inputArray[1]);
-			document.getElementById('rand_number').innerHTML = you_number;
-			inputArray.length = [];
-		}
-	}
-	if(you_number >= 10 && you_number <= 99) {
-		compareNumbers();
-	}
-}
-
-function compareNumbers() {
-	if(you_number == numArray[numArray.length - 2]) {
+	document.getElementById("start").onclick = function() {
 		randNumber();
 	}
-	else {
-		alert("Lose");
+
+	function randNumber() {
+		var num = Math.floor(Math.random() * (100 - 10)) + 10;
+		numArray.push(num);
+		document.getElementById('rand_number').innerHTML = num;
 	}
 	
+	var timer = function() { 
+		intervalId = setInterval(function() {
+	        alert("No time");
+	    },3000);
+	}
+
+	document.onkeydown = function(e) {
+		var key = e.charCode || e.keyCode || 0;
+		var input_number = String.fromCharCode(e.keyCode);
+		if((key >= 48 && key <= 57) || (key >= 96 && key <= 105)) {
+			if(inputArray.length < 2) {
+				inputArray.push(input_number);
+				you_number = inputArray[0];
+				document.getElementById('rand_number').innerHTML = you_number;
+			}
+			if (inputArray.length == 2) {
+				you_number = you_number.concat(inputArray[1]);
+				document.getElementById('rand_number').innerHTML = you_number;
+				inputArray.length = [];
+			}
+		}
+		if(you_number >= 10 && you_number <= 99) {
+			compareNumbers();
+		}
+	}
+
+	function compareNumbers() {
+		if(you_number == numArray[numArray.length - 2]) {
+			clearInterval(intervalId);
+			randNumber();
+			timer();
+		}
+		else {
+			alert("Lose");
+		}
+	
+	}
 }
+
